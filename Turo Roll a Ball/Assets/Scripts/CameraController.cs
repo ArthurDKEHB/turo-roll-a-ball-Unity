dd794c3c7d7532555d250dff6712e5e80daf1613
ArthurDKEHB <arthur.de.keersmaeker@student.ehb.be>
2020-02-17 18:42:00 +0100

    First roll a ball commit

---
+++ Turo Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() //same as update but waits until all items has been processed before updating
+    {
+        transform.position = player.transform.position + offset;
+        
+    }
+}
